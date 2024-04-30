99c2c5f3fee8ca3a28c561392621f156f580b543
Zach <95945017+1012zach@users.noreply.github.com>
2024-04-29 20:42:32 -0500

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,14 @@
+# dwm autostart script
+# written by 1012zach on github
+
+#!/bin/bash
+
+# put the programs you want to autostart with a "&" after the program's command
+
+feh --bg-fill /home/zach/Pictures/gruvbox-fox.png &
+
+mate-polkit &
+
+picom &
+
+slstatus &
